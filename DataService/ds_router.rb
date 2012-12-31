b37c3e9dc74dbb50412bdc5c35102c416c2d4ceb
James <jamesrweaver@gmail.com>
2012-12-31 00:32:24 -0800

    Ran into some issues with changes made for Cross domain. Cleaned up

--- DataService/ds_router.rb
+++ DataService/ds_router.rb
@@ -17,7 +17,8 @@ include ServiceLogger
 # http(s):[Domain]/[UID]/[Namespace]/[Entity]/{attributes}/Method?
 
 set :allow_methods, [:get, :delete, :post, :put, :options]
-
+set :protection, :origin_whitelist => ['http://localhost:8888']
+  
 #Important! - prevents cross domain issues.
 configure do
   enable :cross_origin
@@ -26,24 +27,62 @@ end
 
 get '/hello/:name', :provides => :json do
   pass unless request.accept? 'application/json'
+#  showDetails
   ServiceLogger.log(LOG_INFO, __method__, "Hello #{params[:name]} in jSON - GET".to_s)
   "Hello #{params[:name]} in jSON - GET".to_json
 end
 
 post '/hello/:name', :provides => :json do
   pass unless request.accept? 'application/json'
+#  showDetails
+  ServiceLogger.log(LOG_INFO, __method__, "Hello #{params[:name]} in jSON - POST".to_json.to_s)
   "Hello #{params[:name]} in jSON - POST".to_json
-#  ServiceLogger.log(LOG_INFO, __method__, "Hello #{params[:name]} in jSON - POST".to_json.to_s)
+
+end
+
+put '/hello/:name', :provides => :json do
+  pass unless request.accept? 'application/json'
+#  showDetails
+  ServiceLogger.log(LOG_INFO, __method__, "Hello #{params[:name]} in jSON - PUT".to_json.to_s)
+  "Hello #{params[:name]} in jSON - PUT".to_json
+
 end
 
 delete '/hello/:name', :provides => :json do
-pass unless request.accept? 'application/json'
+  pass unless request.accept? 'application/json'
+#  showDetails
+  ServiceLogger.log(LOG_INFO, __method__, "Hello #{params[:name]} in jSON - delete".to_s)
   "Hello #{params[:name]} in jSON - delete".to_json
-#  ServiceLogger.log(LOG_INFO, __method__, "Hello #{params[:name]} in jSON - delete".to_s)
+
 end
 
 get '/hello/:name', :provides => :xml do
   pass unless request.accept? 'application/xml'
+#  showDetails
+  ServiceLogger.log(LOG_INFO, __method__, "Hello #{params[:name]} in XML!".to_s)
   "Hello #{params[:name]} in XML!".to_xml
-#  ServiceLogger.log(LOG_INFO, __method__, "Hello #{params[:name]} in XML!".to_s)
+end
+
+
+def showDetails
+  puts request.accept               # ['text/html', '*/*']
+  puts request.preferred_type  # 'text/html'
+  puts request.body                # request body sent by the client (see below)
+  puts request.scheme              # "http"
+  puts request.script_name         # "/example"
+  puts  request.path_info           # "/foo"
+  puts request.port                # 80
+  puts request.request_method      # "GET"
+  puts request.query_string        # ""
+  puts request.content_length      # length of request.body
+  puts request.media_type          # media type of request.body
+  puts request.host                # "example.com"
+  #request["SOME_HEADER"]      # value of SOME_HEADER header
+  puts request.referrer            # the referrer of the client or '/'
+  puts request.user_agent          # user agent (used by :agent condition)
+  puts request.cookies             # hash of browser cookies
+  #request.xhr?                # is this an ajax request?
+  puts request.url                 # "http://example.com/example/foo"
+  puts request.path                # "/example/foo"
+  puts request.ip                  # client IP address
 end
--- DataService/main.rb
+++ DataService/main.rb
@@ -21,4 +21,3 @@ helpers do
   
     
 end
-
